2fc6dce0409e3b341a1f074c9bd5fe9bc0143b97
David Wright <24604124+dimwight@users.noreply.github.com>
2021-10-31 12:06:29 +0000

    FORMAT_TIMESTAMP_HTTP

--- src/main/java/org/javarosa/xpath/expr/XPathFuncExpr.java
+++ src/main/java/org/javarosa/xpath/expr/XPathFuncExpr.java
@@ -759,7 +759,9 @@ public class XPathFuncExpr extends XPathExpression {
         } else if (o instanceof String) {
             val = (String) o;
         } else if (o instanceof Date) {
-            val = DateUtils.formatDate((Date) o, DateUtils.FORMAT_ISO8601);
+            //For 637
+            val = false ? DateUtils.formatDate((Date) o, DateUtils.FORMAT_ISO8601)
+                    : DateUtils.formatDateTime((Date) o, DateUtils.FORMAT_TIMESTAMP_HTTP);
         } else if (o instanceof IExprDataType) {
             val = ((IExprDataType) o).toString();
         }
